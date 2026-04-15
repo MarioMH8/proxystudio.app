6092c61171aa71c5b03cf50a46787f87eb87daf3
MarioMH8 <mhidalgomario@gmail.com>
2026-04-15 22:55:44 +0200

    feat(label): enhance Label component with additional font properties and improved structure

--- src/components/label.tsx
+++ src/components/label.tsx
@@ -1,10 +1,10 @@
+import type { FontVariantsProperties } from '@components/font';
 import font from '@components/font';
 import type { VariantProperties } from '@shared/cva';
 import { cn, cva } from '@shared/cva';
 import type { PropertiesWithAsChild } from '@shared/types';
 import { Label as RadixLabel, Slot } from 'radix-ui';
 import type { ReactElement, ReactNode } from 'react';
-import { Children } from 'react';
 
 const variants = cva({
 	base: 'block',
@@ -13,30 +13,34 @@ const variants = cva({
 	variants: {},
 });
 
-type LabelProperties = PropertiesWithAsChild<RadixLabel.LabelProps & VariantProperties<typeof variants>> & {
+type LabelProperties = PropertiesWithAsChild<
+	FontVariantsProperties & RadixLabel.LabelProps & VariantProperties<typeof variants>
+> & {
 	children: [string, ReactElement];
 };
 
-function Label({ asChild = false, children, className, ...properties }: LabelProperties): ReactNode {
-	const Comp = asChild ? Slot.Slot : RadixLabel.Label;
-	const strings = Children.toArray(children).filter(
-		chi => typeof chi === 'string' || typeof chi === 'number' || typeof chi === 'bigint'
-	);
-	const nonStrings = Children.toArray(children).find(
-		chi => typeof chi !== 'string' && typeof chi !== 'number' && typeof chi !== 'bigint'
-	);
+function Label({
+	asChild = false,
+	className,
+	dimension = 'sm',
+	leading,
+	tracking,
+	uppercase,
+	variant = 'muted',
+	weight = 'medium',
+	...properties
+}: LabelProperties): ReactNode {
+	const Comp = asChild ? Slot.Slot : 'label';
 
 	return (
 		<Comp
 			className={cn(
-				font({ dimension: 'sm', variant: 'muted', weight: 'medium' }),
+				font({ dimension, leading, tracking, uppercase, variant, weight }),
 				variants({ className }),
 				className
 			)}
-			{...properties}>
-			{strings.length > 0 && <span>{strings}</span>}
-			<Slot.Slot className='mt-2'>{nonStrings}</Slot.Slot>
-		</Comp>
+			{...properties}
+		/>
 	);
 }
 
