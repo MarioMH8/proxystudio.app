cf689d662890756e62b34334623389b1ed229c20
MarioMH8 <mhidalgomario@gmail.com>
2026-05-04 17:29:49 +0200

    feat(card): add default dimensions to card
    *   Introduce `CardDimensions` interface to define card height and width.
    *   Add default dimensions to the `Card.default` factory function.
    *   Initialize `CardDimensions` with specific height and width values for new cards.

--- src/modules/card/domain/card.ts
+++ src/modules/card/domain/card.ts
@@ -7,7 +7,13 @@ interface CardMetadata {
 	updatedAt: Date;
 }
 
+interface CardDimensions {
+	height: number;
+	width: number;
+}
+
 interface Card {
+	dimensions: CardDimensions;
 	id: string;
 	layers: Layer[];
 	metadata: CardMetadata;
@@ -15,10 +21,15 @@ interface Card {
 }
 
 const Card = {
-	default: ({ layers = [], metadata = {}, ...partial }: DeepPartial<Card> = {}): Card => {
+	default: ({ dimensions = {}, layers = [], metadata = {}, ...partial }: DeepPartial<Card> = {}): Card => {
 		const id = partial.id ?? crypto.randomUUID();
 
 		return {
+			dimensions: {
+				height: 2814,
+				width: 2010,
+				...dimensions,
+			},
 			id,
 			layers: layers.filter(layer => Layer.isLayer(layer)).map(partial => Layer.default(partial)),
 			metadata: {
