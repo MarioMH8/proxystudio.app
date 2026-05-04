4fdbe39e8b9f991a9c5ad5072d28a37e9b7785a0
MarioMH8 <mhidalgomario@gmail.com>
2026-05-05 00:16:26 +0200

    fix(editor): remove unnecessary card rendering from viewport

--- src/modules/editor/presentation/viewport.tsx
+++ src/modules/editor/presentation/viewport.tsx
@@ -1,15 +1,13 @@
-import { CardRenderer } from '@modules/card/presentation';
 import type { VariantProperties } from '@shared/cva';
 import { cn } from '@shared/cva';
 import { cva } from 'cva';
 import type { HTMLAttributes, ReactNode } from 'react';
 import { Panel } from 'react-resizable-panels';
 
-import { useEditorContext } from '../store';
 import EditorViewportToolbar from './viewport-toolbar';
 
 const variants = cva({
-	base: 'relative',
+	base: 'relative overflow-hidden',
 	compoundVariants: [],
 	defaultVariants: {},
 	variants: {},
@@ -18,14 +16,11 @@ const variants = cva({
 type EditorViewportProperties = Omit<HTMLAttributes<HTMLDivElement>, 'onResize'> & VariantProperties<typeof variants>;
 
 function EditorViewport({ children, className, ...properties }: EditorViewportProperties): ReactNode {
-	const { card } = useEditorContext();
-
 	return (
 		<Panel
 			className={cn(variants({ className }), className)}
 			id='editor-viewport'
 			{...properties}>
-			<CardRenderer card={card} />
 			<EditorViewportToolbar />
 		</Panel>
 	);
