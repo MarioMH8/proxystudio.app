fd8a91caac41f92acb3f2d869d696fce4c3930ef
MarioMH8 <mhidalgomario@gmail.com>
2026-05-04 17:30:03 +0200

    feat(card): add card renderer component
    *   Introduce `CardRenderer` component for rendering cards.
    *   Add `CardRenderer` to the `card/presentation/index` export.

---
+++ src/modules/card/presentation/card-renderer.tsx
@@ -0,0 +1,19 @@
+import type { Card } from '@modules/card/domain';
+import type { ReactNode } from 'react';
+
+interface CardRendererProperties {
+	card: Card;
+}
+
+function CardRenderer({ card }: CardRendererProperties): ReactNode {
+	return (
+		<canvas
+			height={card.dimensions.height}
+			width={card.dimensions.width}
+		/>
+	);
+}
+
+CardRenderer.displayName = 'CardRenderer';
+
+export default CardRenderer;
