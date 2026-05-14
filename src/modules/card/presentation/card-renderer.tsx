39fad38f473a757ae478ff04315e35adf60c24b3
MarioMH8 <mhidalgomario@gmail.com>
2026-05-14 16:07:08 +0200

    feat: include export image in ref

--- src/modules/card/presentation/card-renderer.tsx
+++ src/modules/card/presentation/card-renderer.tsx
@@ -1,20 +1,39 @@
 import type { Card } from '@modules/card/domain';
-import type { ReactNode } from 'react';
+import { forwardRef, useImperativeHandle, useRef } from 'react';
 
 interface CardRendererProperties {
 	card: Card;
 }
 
-function CardRenderer({ card }: CardRendererProperties): ReactNode {
+interface CardRendererReference {
+	exportImage: () => string;
+}
+
+const CardRenderer = forwardRef<CardRendererReference, CardRendererProperties>(({ card }, reference) => {
+	const canvasReference = useRef<HTMLCanvasElement>(null);
+
+	useImperativeHandle(reference, () => ({
+		exportImage() {
+			if (!canvasReference.current) {
+				throw new Error('CardRenderer canvas is not available');
+			}
+
+			return canvasReference.current.toDataURL('image/png');
+		},
+	}));
+
 	return (
 		<canvas
 			className='bg-[#FABADA]'
 			height={card.dimensions.height}
+			ref={canvasReference}
 			width={card.dimensions.width}
 		/>
 	);
-}
+});
 
 CardRenderer.displayName = 'CardRenderer';
+
+export type { CardRendererReference };
 
 export default CardRenderer;
